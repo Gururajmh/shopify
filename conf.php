8a5bd0230bf32a691b023dfe4767403773145194
guru <dingdongdung9@gmail.com>
2017-08-30 19:22:09 +0530

    Hello ram

--- conf.php
+++ conf.php
@@ -3,4 +3,4 @@
 	define('SHOPIFY_APP_API_KEY', 'cf2dcbf94c45455d9e5811cc7d4926c7');
 	define('SHOPIFY_APP_SHARED_SECRET', '61b4f91ba0a8a63aa519a16130601bda');
 
-define('URL', 'http://localhost/guru/oauth.php');
+define('URL', 'http://ut.hereandnowhiring.com/guru/oauth.php');
