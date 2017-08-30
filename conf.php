96adebc0b8e5d203240abb024bbbce11e2fa6a0d
guru <dingdongdung9@gmail.com>
2017-08-30 18:27:40 +0530

    1st Commit

---
+++ conf.php
@@ -0,0 +1,6 @@
+<?php
+
+	define('SHOPIFY_APP_API_KEY', 'cf2dcbf94c45455d9e5811cc7d4926c7');
+	define('SHOPIFY_APP_SHARED_SECRET', '61b4f91ba0a8a63aa519a16130601bda');
+
+define('URL', 'http://localhost/guru/oauth.php');
