5667f3ce38ea226cd03431978e7f7e8ef5826246
guru <dingdongdung9@gmail.com>
2017-09-01 16:50:46 +0530

    orders

--- get_shop.php
+++ get_shop.php
@@ -11,7 +11,7 @@
 	try
 	{
 		# Making an API request can throw an exception
-		$shop = $shopify('GET /admin/order.json');
+		$shop = $shopify('GET /admin/orders.json');
 		print_r($shop);
 
     $connect=mysqli_connect("localhost","root","","shopify");
